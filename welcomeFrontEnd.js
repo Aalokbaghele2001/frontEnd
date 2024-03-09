37e739f30ba1a8e42dc84ca7f217b5962d68cb6a
Aalokbaghele2001 <aalokebaghele@gmail.com>
2024-03-10 02:50:13 +0530

    commit

---
+++ welcomeFrontEnd.js
@@ -0,0 +1,9 @@
+// const str='The 1 Quick Brown 4 Fox Jumps 4 Over The Lazzy 2 Dog It Barked. the hhh #$%^&*^$$';
+// console.log(str.match(/[A-Z]/g));
+// console.log(str.match(/[a-z]/g));
+// console.log(str.match(/[0-9]/g));
+// console.log(str);
+
+// console.log(str.replaceAll(/[^A-Z]/g,""));
+
+
